256d898045fbcb4151989e890cb68c49ec4f8c1d
Nikhil Tyagi <nikhiltyagi.54@gmail.com>
2026-02-03 11:23:02 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nikhil Tyagi on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Nikhil Tyagi on 03/02/26.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
